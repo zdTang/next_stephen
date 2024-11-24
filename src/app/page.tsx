6203e79a1a4fb8a51e4eb4d0038b0dd1e8a6e130
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-24 10:00:30 -0500

    1-10-2 The second Way to display Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import HomeImg from "../../public/home.jpg";
 
 export default function Home() {
   return (
     <div>
       Home Page
       <Image
-        src="/home.jpg"
+        src={HomeImg}
         alt="Car Factory"
         fill
         style={{ objectFit: "cover" }}
