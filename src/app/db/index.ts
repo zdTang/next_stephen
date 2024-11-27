53eee0867ddecb5700e79a9398a077322e39c08e
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-11-27 15:18:46 -0500

    2-20 Install Prisma and create Prisma Client

---
+++ src/app/db/index.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+export const db = new PrismaClient();
+
+db.snippet.create({
+  data: {
+    title: "Title!",
+    code: "const abc=()=>{}",
+  },
+});
