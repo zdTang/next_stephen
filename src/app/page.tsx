43c55e77ceed77137294894d1795c6b65ec14314
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-12-18 14:25:32 -0500

    3-30 A Few Quick Tasks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,30 @@
+import Link from "next/link";
 import { db } from "@/db";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
   const renderedSnippets = snippets.map((snippet) => {
     return (
-      <div key={snippet.id}>
-        <h2>{snippet.title}</h2>
-      </div>
+      <Link
+        key={snippet.id}
+        className="flex justify-between items-center p-2 border rounded"
+        href={`/snippets/${snippet.id}`}
+      >
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
     );
   });
 
-  return <div>{renderedSnippets}</div>;
+  return (
+    <div>
+      <div className="flex m-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link className="border border-rounded p-2" href="/snippets/new">
+          New Snippet
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
+    </div>
+  );
 }
--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -6,6 +6,8 @@ interface SnippetShowPageProps {
   };
 }
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  await new Promise((resolve) => setTimeout(resolve, 2000)); // to simulate a loading with a artificial delay
+
   const { id } = await props.params;
   const snippet = await db.snippet.findFirst({
     where: { id: parseInt(id) },
@@ -17,7 +19,7 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
 
   return (
     <div>
-      <h1>snippit</h1>
+      <h1>{snippet.code}</h1>
     </div>
   );
 }
