9896fbea5f44e561df63939fd3ed4c775a426dac
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-12-18 09:51:58 -0500

    3-27 Fetching particular records

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,8 +1,23 @@
-export default function SnippetShowPage(props: any) {
-  console.log(props);
+import { notFound } from "next/navigation";
+import { db } from "@/db";
+interface SnippetShowPageProps {
+  params: {
+    id: string;
+  };
+}
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  const { id } = await props.params;
+  const snippet = await db.snippet.findFirst({
+    where: { id: parseInt(id) },
+  });
+  console.log(snippet);
+  if (!snippet) {
+    return notFound();
+  }
+
   return (
     <div>
-      <h1>Snippet Show Page</h1>
+      <h1>snippit</h1>
     </div>
   );
 }
