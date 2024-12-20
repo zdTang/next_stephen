03aacb0db9a1c5fdb1e5ba94282dbf445da69ee0
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-12-20 15:25:13 -0500

    4-42 useActionState in action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -15,3 +15,23 @@ export async function deleteSnippet(id: number) {
   });
   redirect(`/`);
 }
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  /* // Check the user's inputs and make sure they are valid
+  const title = formData.get("title") as string;
+  const code = formData.get("code") as string;
+  // Create a new record in the database
+  const snippet = await db.snippet.create({
+    data: { title, code },
+  });
+  console.log(snippet);
+  // Redirect the user back to the root route
+  redirect("/"); */
+
+  return {
+    message: "Title must be longer",
+  };
+}
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,23 +1,15 @@
+"use client";
 import { db } from "@/db";
 import { redirect } from "next/navigation";
-export default function SnippetCreatePage() {
-  async function createSnippet(formData: FormData) {
-    // This need to be a server action!
-    "use server";
-    // Check the user's inputs and make sure they are valid
-    const title = formData.get("title") as string;
-    const code = formData.get("code") as string;
-    // Create a new record in the database
-    const snippet = await db.snippet.create({
-      data: { title, code },
-    });
-    console.log(snippet);
-    // Redirect the user back to the root route
-    redirect("/");
-  }
+import { useActionState } from "react";
+import * as actions from "@/actions";
 
+export default function SnippetCreatePage() {
+  const [state, SnippetFormAction] = useActionState(actions.createSnippet, {
+    message: "",
+  });
   return (
-    <form action={createSnippet}>
+    <form action={SnippetFormAction}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -41,7 +33,7 @@ export default function SnippetCreatePage() {
             id="code"
           />
         </div>
-
+        <div>{state.message}</div>
         <button type="submit" className="rounded p-2 bg-blue-200">
           Create
         </button>
