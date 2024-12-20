a1d016f2d4ae6515fe93afd232309ceb3a784fd0
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-12-20 17:16:24 -0500

    4-44 Gotchas Around Error Handling

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -17,29 +17,40 @@ export async function deleteSnippet(id: number) {
 }
 
 export async function createSnippet(
-  formState: { message: string },
+  currentState: { message: string },
   formData: FormData
 ) {
-  // Check the user's inputs and make sure they are valid
-  const title = formData.get("title");
-  const code = formData.get("code");
+  try {
+    // Check the user's inputs and make sure they are valid
+    const title = formData.get("title");
+    const code = formData.get("code");
 
-  if (typeof title !== "string" || title.length < 3) {
-    return {
-      message: "Title must be longer",
-    };
-  }
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title must be longer",
+      };
+    }
 
-  if (typeof code !== "string" || code.length < 3) {
-    return {
-      message: "Code must be longer",
-    };
+    if (typeof code !== "string" || code.length < 3) {
+      return {
+        message: "Code must be longer",
+      };
+    }
+    // Create a new record in the database
+    const snippet = await db.snippet.create({
+      data: { title, code },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        message: error.message,
+      };
+    } else {
+      return {
+        message: "Something went wrong",
+      };
+    }
   }
-  // Create a new record in the database
-  const snippet = await db.snippet.create({
-    data: { title, code },
-  });
-  console.log(snippet);
   // Redirect the user back to the root route
   redirect("/");
 }
