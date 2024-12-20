c6f494b48ffa34ecdc550592b8e1cf44e734e87b
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-12-19 20:57:08 -0500

    3-34 Adding the Monaco Editor

--- src/app/components/snippet-edit-form.tsx
+++ src/app/components/snippet-edit-form.tsx
@@ -1,9 +1,23 @@
 "use client";
 import type { Snippet } from "@prisma/client";
+import Editor from "@monaco-editor/react";
 
 interface SnippetNewPageProps {
   snippet: Snippet;
 }
 export default function SnippetEditForm({ snippet }: SnippetNewPageProps) {
-  return <div>Client component.</div>;
+  function handleEditorChange(value: any, event: Event) {
+    console.log("here is the current model value:", value);
+  }
+
+  return (
+    <Editor
+      height="40vh"
+      theme="vs-dark"
+      defaultLanguage="javascript"
+      defaultValue={snippet.code}
+      options={{ minimap: { enabled: false } }}
+      onChange={handleEditorChange}
+    />
+  );
 }
