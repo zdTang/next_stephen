04d2b7dee290ea617b3867ee68a2e5af2118bb81
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-12-24 22:29:20 -0500

    revert to stephen's version

--- src/app/snippets/[id]/edit/page.tsx
+++ src/app/snippets/[id]/edit/page.tsx
@@ -1,31 +1,26 @@
 import { notFound } from "next/navigation";
 import { db } from "@/db";
 import SnippetEditForm from "@/components/snippet-edit-form";
+
 interface SnippetEditPageProps {
-  params:
-    | Promise<{
-        id: string;
-      }>
-    | {
-        id: string;
-      };
+  params: {
+    id: string;
+  };
 }
 
-const Page = async (props: SnippetEditPageProps) => {
-  const snippetParams = props.params; // Ensure params are awaited
-  const id = parseInt(snippetParams.id, 10); // Parse the id as an integer
-
+export default async function SnippetEditPage(props: SnippetEditPageProps) {
+  const id = parseInt(props.params.id);
   const snippet = await db.snippet.findFirst({
     where: { id },
   });
 
-  if (!snippet) return notFound();
+  if (!snippet) {
+    return notFound();
+  }
 
   return (
     <div>
       <SnippetEditForm snippet={snippet} />
     </div>
   );
-};
-
-export default Page;
+}
